1b86163724b50855b796b4d3f1a01db78141ea31
gahyunkim <mapledt001@naver.com>
2023-06-28 15:52:16 +0900

    BottomNavigationView+Toolbar+MainFrag

--- app/src/main/java/com/example/teaming/MainActivity.kt
+++ app/src/main/java/com/example/teaming/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.teaming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.teaming.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val mainFragment by lazy { MainFragment() }
+    private val calFragment by lazy { CalFragment() }
+    private val notiFragment by lazy { NotiFragment() }
+    private val fileFragment by lazy { FileFragment() }
+    private val myFragment by lazy { MyFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigationBar()
+    }
+
+    private fun initNavigationBar() {
+        binding.bottomNavi.run{
+            setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.home -> changeFragment(mainFragment)
+                    R.id.cal -> changeFragment(calFragment)
+                    R.id.noti -> changeFragment(notiFragment)
+                    R.id.file -> changeFragment(fileFragment)
+                    R.id.my -> changeFragment(myFragment)
+                }
+                true
+            }
+            selectedItemId = R.id.home
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.container,fragment).commit()
     }
 }
