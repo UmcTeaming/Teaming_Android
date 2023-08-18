22d279720a6bb53fe2e45b5e8148e4af5f7ed872
gahyunkim <mapledt001@naver.com>
2023-08-18 14:14:32 +0900

    Post 연결중 CreateFragment - 프래그먼트 내부에 데이터가 다 들어갔을 때 버튼 활성화 등의 내용 구성

--- app/src/main/java/com/example/teaming/RetrofitService.kt
+++ app/src/main/java/com/example/teaming/RetrofitService.kt
@@ -5,12 +5,10 @@ import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 /**
  * API Request를 Interface로 정의하는 곳입니다.
@@ -32,11 +30,9 @@ interface RetrofitService {
     @Multipart
     @POST("/projects/{memberId}/create")
     fun createProject(
-        @Path("memberId") memberId: Int?,
-        @Part("project_name") projectName: RequestBody,
-        @Part image: MultipartBody.Part,
-        @Part("start_date") startDate: RequestBody,
-        @Part("end_date") endDate: RequestBody,
-        @Part("project_color") projectColor: RequestBody
+        @Path("memberId") memberId: Int,
+        @Part("data") requestData: RequestBody,
+        @Part projectImage: MultipartBody.Part?
     ): Call<CreateProjectResponse>
+
 }
