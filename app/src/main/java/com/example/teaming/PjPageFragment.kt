ad61e337541255fc6c6f294db6bdc53ee18fd1b4
manwoong <cheonwoong21@naver.com>
2023-08-23 03:18:49 +0900

    멤버쪽 관련해서 고침

--- app/src/main/java/com/example/teaming/PjPageFragment.kt
+++ app/src/main/java/com/example/teaming/PjPageFragment.kt
@@ -304,14 +304,12 @@ class PjPageFragment : Fragment() {
 
             val memberId = sharedPreference_mem.getInt("memberId",-1)
 
-            val projectId = sharedPreference.getInt("projectID_page",-1)
-
             val requestBodyData = InvitationsRequest(dialogBinding.emailWrite.text.toString())
             Log.d("리퀘스트","${requestBodyData}")
             val json = Gson().toJson(requestBodyData)
             val requestBody = RequestBody.create("application/json".toMediaType(), json)
 
-            val callInvitation = RetrofitApi.getRetrofitService.invitation(memberId,projectId,requestBody)
+            val callInvitation = RetrofitApi.getRetrofitService.invitation(memberId, projectIdAll,requestBody)
 
             callInvitation.enqueue(object : Callback<InvitationsResponse> {
                 override fun onResponse(call: Call<InvitationsResponse>, response: Response<InvitationsResponse>) {
@@ -349,7 +347,8 @@ class PjPageFragment : Fragment() {
                             }
                         }
                     } else {
-                        Log.d("Invitation", "API 호출 실패: ${response.code()}")
+                        pjInviteDialog.dismiss()
+                        showInviteNoInfoDialog(response.code())
                     }
                 }
 
@@ -376,10 +375,6 @@ class PjPageFragment : Fragment() {
 
         dialogBinding.closeYesBtn.setOnClickListener {
 
-            sleep(300)
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.container,PjPageFragment())
-                .commit()
 
             inviteYesInfoDialog.dismiss()
 
@@ -400,6 +395,7 @@ class PjPageFragment : Fragment() {
         if (errorcode == 208){
             dialogBinding.noWayHome.text = "이미 참여 중인 초대자입니다."
         }else if(errorcode == 404){
+            Log.d("뭐지","404")
             dialogBinding.noWayHome.text = "회원이 아닌 초대자 입니다."
         }
         inviteNoInfoDialog.setContentView(dialogBinding.root)
