0408b33f3ad124e896953962d4e70d4d885c6c84
kwak_eunjae <emma_0407@naver.com>
2023-08-23 13:31:38 +0900

    eunjae commit and push

--- app/src/main/java/com/example/teaming/PjModiDialog.kt
+++ app/src/main/java/com/example/teaming/PjModiDialog.kt
@@ -3,6 +3,7 @@ package com.example.teaming
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,10 +28,32 @@ class PjModiDialog(): DialogFragment() {
         // 확인 버튼
         binding.btnYes.setOnClickListener{
             dismiss()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.container,MainFragment())
+                .addToBackStack(null)
+                .commit()
         }
 
-        // 프로젝트 보러가기 버튼
         binding.btnPj.setOnClickListener{
+            val projectId = arguments?.getInt("modiProjectID")
+            //val projectId3 = arguments?.getInt("createProjectID")
+            val num = arguments?.getInt("num")
+            /*val num3 = arguments?.getInt("num")*/
+
+            //Log.e("넘긴 값들","${projectId},${projectId3}, ${num}")
+
+            val bundle = Bundle()
+            bundle.putInt("modiProjectID",projectId!!)
+            bundle.putInt("num",num!!)
+            //bundle.putInt("num",num3!!)
+
+            val pjPageFragment = PjPageFragment()
+            pjPageFragment.arguments = bundle
+
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.container,pjPageFragment)
+                .addToBackStack(null)
+                .commit()
             dismiss()
         }
 
